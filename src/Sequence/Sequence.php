e9201c6bec758c77481090c7ee4473fee96b94cb
Kanstantsin Mesnik <haspadar@gmail.com>
2025-11-13 12:43:24 +0300

    feat: introduce Sequence interface

---
+++ src/Sequence/Sequence.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 Kanstantsin Mesnik
+ * SPDX-License-Identifier: MIT
+*/
+declare(strict_types=1);
+
+namespace Primus\Sequence;
+
+use IteratorAggregate;
+use Traversable;
+
+/**
+ * A sequence of values that can be iterated in a uniform way.
+ *
+ * @template T
+ * @extends IteratorAggregate<int, T>
+ *
+ * @since 0.1
+ */
+interface Sequence extends IteratorAggregate
+{
+    /**
+     * @return \Iterator<int, T>
+     */
+    #[\Override]
+    public function getIterator(): Traversable;
+}
