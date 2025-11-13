c9cbfcdbf1cceafaae4d72e97170aadd8b7b5a63
Kanstantsin Mesnik <haspadar@gmail.com>
2025-11-13 12:48:10 +0300

    feat(sequence): add SequenceOf for Reduced

---
+++ src/Sequence/SequenceOf.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 Kanstantsin Mesnik
+ * SPDX-License-Identifier: MIT
+ */
+declare(strict_types=1);
+
+namespace Primus\Sequence;
+
+use ArrayIterator;
+use Iterator;
+
+/**
+ * A sequence backed by a PHP array.
+ *
+ * Example:
+ * $seq = new SequenceOf(['a', 'b', 'c']);
+ * foreach ($seq as $item) {
+ *     echo $item; // a b c
+ * }
+ *
+ * @template T
+ * @implements Sequence<T>
+ *
+ * @since 0.1
+ */
+final readonly class SequenceOf implements Sequence
+{
+    /**
+     * @param list<T> $items
+     */
+    public function __construct(private array $items)
+    {
+    }
+
+    /** @return Iterator<int, T> */
+    #[\Override]
+    public function getIterator(): Iterator
+    {
+        return new ArrayIterator($this->items);
+    }
+
+    /** @return list<T> */
+    public function value(): array
+    {
+        return $this->items;
+    }
+}
