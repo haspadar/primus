5fb6ff173f7ba149ce29110062b5971924941efe
Konstantinas Mesnikas <haspadar@gmail.com>
2026-05-11 17:16:22 +0300

    #141: Inherit envelope constructor where the child only forwards origin

--- src/List/Reversed.php
+++ src/List/Reversed.php
@@ -23,16 +23,6 @@ use Override;
  */
 final readonly class Reversed extends ListEnvelope
 {
-    /**
-     * Ctor.
-     *
-     * @param List_<T> $origin The list to reverse.
-     */
-    public function __construct(List_ $origin)
-    {
-        parent::__construct($origin);
-    }
-
     #[Override]
     public function value(): array
     {
