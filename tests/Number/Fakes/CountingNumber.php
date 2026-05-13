0cdae23f5940ecc2fb2492200e7f5a5426434eb6
Konstantinas Mesnikas <haspadar@gmail.com>
2026-05-13 12:32:38 +0300

    #173: Cache Number projections to avoid retraversing deep decorator trees

---
+++ tests/Number/Fakes/CountingNumber.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Primus\Tests\Number\Fakes;
+
+use Override;
+use Primus\Number\Number;
+
+/**
+ * Number fake that counts how many times each projection is invoked.
+ *
+ * Used to verify caching behaviour of decorators such as {@see \Primus\Number\Sticky}.
+ */
+final class CountingNumber implements Number
+{
+    public int $intCalls = 0;
+    public int $floatCalls = 0;
+
+    public function __construct(private readonly int $intValue, private readonly float $floatValue) {}
+
+    #[Override]
+    public function asInt(): int
+    {
+        ++$this->intCalls;
+
+        return $this->intValue;
+    }
+
+    #[Override]
+    public function asFloat(): float
+    {
+        ++$this->floatCalls;
+
+        return $this->floatValue;
+    }
+}
